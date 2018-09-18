7bb231b31499dc3bd1bdc74c96df54a5ac1519fa
marenjifo <marialejarenjifo@hotmail.com>
2018-09-18 13:18:24 -0500

    PrintWriter Añadido

--- app/src/main/java/com/example/a1107516259/tcp_s8/Cliente.java
+++ app/src/main/java/com/example/a1107516259/tcp_s8/Cliente.java
@@ -3,6 +3,9 @@ package com.example.a1107516259.tcp_s8;
 import android.util.Log;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Cliente extends Thread{
@@ -12,6 +15,11 @@ public class Cliente extends Thread{
         //Del emulador a servidor externo
         try {
             Socket s =new Socket("10.0.2.2",5000);
+            OutputStream os= s.getOutputStream();
+            PrintWriter out=new PrintWriter(new OutputStreamWriter(os));
+            out.println("Hoda ya funciona :)");
+            out.flush();
+
             Log.e("DEBUG","Conexion exitosa");
         } catch (IOException e) {
             e.printStackTrace();
